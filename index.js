8695f0b878eb21f281596c528c827b073691060b
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-08-09 14:48:33 -0500

    testing deployment

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ server.use(express.json())
 
 server.use('/api/videoGames', videoGames)
 
+server.get('/', (req, res) => {
+    res.send('Welcome to My Video Game Server!')
+})
+
 server.use((err, req, res, next) => {
     console.log(err)
     res.status(500).json({ message: 'There was an error performing the operation' })
